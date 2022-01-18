f9f9339916ba7b44341f8a2c5fbbdc527bcdfdca
Brian Chen <brian.chen20@det.nsw.edu.au>
2022-01-18 20:50:06 +1100

    simple cli version info

---
+++ cli/cmd/version.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	goruntime "runtime"
+)
+
+var (
+	AppVersion, BuildDate, GitCommit string
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version",
+	Long:  `Display version and build information about rapidkube.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		printVersion()
+	},
+}
+
+func printVersion() {
+	log.Info(fmt.Sprintf("Version: %s", AppVersion))
+	log.Info(fmt.Sprintf("Go Version: %s", goruntime.Version()))
+	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", goruntime.GOOS, goruntime.GOARCH))
+	log.Info(fmt.Sprintf("Git Commit: %s", GitCommit))
+	log.Info(fmt.Sprintf("BuildDate: %s", BuildDate))
+}
--- cli/main.go
+++ cli/main.go
@@ -17,30 +17,14 @@ limitations under the License.
 package main
 
 import (
-	"fmt"
 	"github.com/dhouti/sops-converter/cli/cmd"
 	"github.com/dhouti/sops-converter/cli/logger"
-	log "github.com/sirupsen/logrus"
-	goruntime "runtime"
-)
-
-var (
-	AppVersion, BuildDate, GitCommit string
 )
 
 func init() {
 	logger.ConfigureLogging(nil)
 }
 
-func printVersion() {
-	log.Info(fmt.Sprintf("Version: %s", AppVersion))
-	log.Info(fmt.Sprintf("Go Version: %s", goruntime.Version()))
-	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", goruntime.GOOS, goruntime.GOARCH))
-	log.Info(fmt.Sprintf("Git Commit: %s", GitCommit))
-	log.Info(fmt.Sprintf("BuildDate: %s", BuildDate))
-}
-
 func main() {
-	printVersion()
 	cmd.Execute()
 }
